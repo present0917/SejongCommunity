8d0937fb260e665e72c3d6fd369ad39fae082fd1
present0917 <guswo0329@gmail.com>
2023-03-19 22:54:15 +0900

    navi complete?

--- src/components/Nav/Footer.js
+++ src/components/Nav/Footer.js
@@ -1,7 +1,21 @@
+import "./FontAwesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import "./Footer.css"
 const Footer = () => {
     return (
       <footer>
-        <h2>This is Footer</h2>
+        <nav className="bottom">
+                <div>
+                    <Link to='./'><FontAwesomeIcon icon="home" /></Link>
+                </div>
+                <div>
+                    <Link to='./search'><FontAwesomeIcon icon="compass" /></Link>
+                </div>
+                <div>
+                    <Link to='./my'><FontAwesomeIcon icon="user" /></Link>
+                </div>
+            </nav>
       </footer>
     )
   }
--- src/components/Nav/Header.js
+++ src/components/Nav/Header.js
@@ -1,13 +1,16 @@
 import './Header.css'
+import "./FontAwesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <header className='header'>
             <div className='contents'>
                 <div>
-                    one
+                <Link to='./search'><FontAwesomeIcon icon="bars" /></Link>
                 </div>
                 <div>
-                    two
+                <Link to='./search'><FontAwesomeIcon icon="compass" /></Link>
                 </div>
             </div>
         </header>
