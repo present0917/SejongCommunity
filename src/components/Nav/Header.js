238b49f1f159d56d54a5a4cb7823abdf265a7669
present0917 <guswo0329@gmail.com>
2023-03-23 20:02:21 +0900

    week3

--- src/components/Nav/Header.js
+++ src/components/Nav/Header.js
@@ -4,25 +4,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import { useState } from 'react';
 const Header = () => {
-    const [buttonClick,setbuttonClick]=useState(false);
-    const test=()=>
-    {
+    const [buttonClick, setbuttonClick] = useState(false);
+    const test = () => {
         setbuttonClick(!buttonClick);
     }
     return (
         <header className='header'>
             <div className='contents'>
                 <div>
-                <FontAwesomeIcon icon={buttonClick ? "xmark" : "bars"} onClick={test} className='bar' />
+                    <FontAwesomeIcon icon={buttonClick ? "xmark" : "bars"} onClick={test} className='bar' />
                 </div>
                 <div>
-                <Link to='./search'><FontAwesomeIcon icon="compass" /></Link>
+                    <Link to='./search'><FontAwesomeIcon icon="compass" /></Link>
                 </div>
             </div>
-            <menu className={buttonClick ? "show" : "hide"}> 
-                <div>hello</div>
-                <div>it's test</div>
+
+
+            <menu className={buttonClick ? "show" : "hide"}>
+                <ul>
+                    <li>menu1</li>
+                    <li>menu2</li>
+                    <li>menu3</li>
+                    <li>menu4</li>
+                    <li>menu5</li>
+                </ul>
+
             </menu>
+
+
         </header>
     )
 }
