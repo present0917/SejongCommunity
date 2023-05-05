163ca4d3acea7494d70d367917bc656b73e4dff2
present0917 <guswo0329@gmail.com>
2023-05-05 13:39:15 +0900

    Merge remote-tracking branch 'origin/master' into present

---
+++ src/etc/List.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { useState } from "react";
+const imgg = styled.img`
+position: relative;
+`;
+const List = ( props ) => {
+  const { image, text, top, left,title } = props.data;
+  const style = { top, left }; // 스타일 객체 생성
+  return (
+    <div className="Crd">
+        <img src={Number(image)} style=
+        {{width:"10vmin", height:"12vmin"}}
+        
+        />
+        <div className="text-container" 
+        onClick={()=>{
+          props.func();
+          
+          }}>
+          {image}
+      </div>
+    </div>
+  );
+};
+export default List;
