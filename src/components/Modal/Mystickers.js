8dc5984df370f269afadefe833853b9bdd66aab7
present0917 <guswo0329@gmail.com>
2023-05-16 21:32:00 +0900

    내스티커 스크롤, 알람구현

--- src/components/Modal/Mystickers.js
+++ src/components/Modal/Mystickers.js
@@ -1,11 +1,9 @@
 import Modal from './Modal';
 import { useState } from 'react';
-
+import { useEffect } from 'react';
 const  Mystickers = (props) => {
     
-  const [name, setName] = useState('');
-  const [text, settext] = useState('');
-  const [memo, setmemo] = useState('');
+  const [usetext, settext] = useState([]);
 
   async function deletemytree() {   //내트리지우기
     const response = await fetch("/stickers");
@@ -17,15 +15,41 @@ const  Mystickers = (props) => {
       throw new Error('No Search Data');
     }
     console.log(data);
+    settext(data.data);
   }
 
-deletemytree();
+// deletemytree();
 
+useEffect(() => {
+    deletemytree();
+  }, []);
 
   return (
-    <Modal onClose={props.onClose} style={{color:"blue"}} >
+    <Modal onClose={props.onClose} style={{overflow:"auto"}} >
     <div style={{color:"blue"}}>
-        
+        test
+        <div>
+        {usetext.map((data) => (
+          <div key={Math.random()} style={{
+            border: "1px solid black"
+          }} >
+            <div>
+            제목{data.title}   
+            </div>
+            <div>
+            내용{data.message}
+            </div>
+            <div>
+            스티커모양{data.type}
+            </div>
+            <div>
+
+            트리번호{data.treeKey}      
+            </div>
+            </div>
+        ))} </div>
+            {/* {usetext.data[0].treeKey} */}
+
         </div>
   </Modal>
   );
--- src/components/Pages/Alert.js
+++ src/components/Pages/Alert.js
@@ -1,26 +1,42 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import PrintFetch from "../http/PrintFetch";
 import Alertlist from "../../etc/Alertlist";
+import { Link } from "react-router-dom";
 const Alert = () => {
     const [testData,setTestData] = useState([])
-    async function fetchtest() {
-        const response = await fetch('https://swapi.dev/api/films');
-        const data = await response.json();
-        const mapping = await data.results.map((element)=>{
-            return{
-                id:element.episode_id,
-                title:element.title,
-            };
-        }
-        );
-        setTestData(mapping);
-    };
-    return (
 
+    async function alarm() {
+        const response = await fetch('/login');
+        const data = await response.json();
+
+       console.log(data);
+        console.log(data.alarmCount);
+        setTestData(Object.entries(data.alarmCount));
+    };
+    
+
+    useEffect(() => {
+        alarm();
+        console.log('실행');
+      }, []);
+
+      useEffect(() => {
+        console.log("Updated testData:", testData);
+      }, [testData]);
+
+    return (
         <div>
-            alert page
-            {/* <button onClick={fetchtest}>fetch</button> */}
-            <Alertlist data={1} key={1}></Alertlist>
+          <ul>
+            {testData.map(([key, value]) => (
+                
+          <li key={key}>
+            <Link to={`/tree/${key}`}>
+            {key}번 트리에 {value}개의 알림이 있습니다.
+            </Link>
+          </li>
+        ))}
+      </ul>
+
         </div>
     )
 }
