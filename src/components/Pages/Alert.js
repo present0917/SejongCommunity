aca0be09c0bb67b30a2399ed21b8897c203073c6
present0917 <guswo0329@gmail.com>
2023-05-19 16:42:07 +0900

    알람형식변경

--- src/components/Pages/Alert.js
+++ src/components/Pages/Alert.js
@@ -2,22 +2,20 @@ import { useState,useEffect } from "react";
 import PrintFetch from "../http/PrintFetch";
 import Alertlist from "../../etc/Alertlist";
 import { Link } from "react-router-dom";
-const Alert = () => {
+const Alert = (props) => {
     const [testData,setTestData] = useState([])
 
     async function alarm() {
         const response = await fetch('/login');
         const data = await response.json();
 
-       console.log(data);
-        console.log(data.alarmCount);
-        setTestData(Object.entries(data.alarmCount));
+
+
     };
     
 
     useEffect(() => {
         alarm();
-        console.log('실행');
       }, []);
 
       useEffect(() => {
@@ -27,11 +25,11 @@ const Alert = () => {
     return (
         <div>
           <ul>
-            {testData.map(([key, value]) => (
+            {testData.map((data) => (
                 
-          <li key={key}>
-            <Link to={`/tree/${key}`}>
-            {key}번 트리에 {value}개의 알림이 있습니다.
+          <li key={Math.random()}>
+            <Link to={`/tree/${data.id}`}>
+            {data.name} 트리에 {data.count}개의 알림이 있습니다.
             </Link>
           </li>
         ))}
