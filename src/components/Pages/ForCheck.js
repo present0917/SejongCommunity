e0df4d29fe5ff327f91e7726ac01152e4a51e0b2
present0917 <guswo0329@gmail.com>
2023-05-19 16:51:11 +0900

    forcheck 이상작동 수정(아래첨부) forcheck에서 context 사용 알람갯수 props 전달
        if (!response.ok) {
          navigate("/Errorlogin");
        }
        const data = await response.json();
        console.log(data);
        if (data.isLogin === false) navigate("/Errorlogin");

--- src/components/Pages/ForCheck.js
+++ src/components/Pages/ForCheck.js
@@ -2,6 +2,7 @@ import { Outlet, Navigate, useLocation, useNavigate } from "react-router-dom";
 import AuthLogin from "../../context/auto-login";
 import { useContext, useEffect } from "react";
 import Errorlogin from "./Error/Errorlogin";
+import { useState } from "react";
 const Forcheck = () => {
   const location = useLocation();
   console.log("forcheck입니다.");
@@ -13,17 +14,40 @@ const Forcheck = () => {
 
     //서버 응답 x
     if (!response.ok) {
-      //navigate("/login");
+      navigate("/Errorlogin");
     }
     const data = await response.json();
     console.log(data);
-    if (data === false) navigate("/Errorlogin");
+    if (data.isLogin === false) navigate("/Errorlogin");
   }
 
+  const [testData,setTestData] = useState([])
+  const [sum,setSum]=useState(0);
+
+  async function alarm() {
+      const response = await fetch('/login');
+      const data = await response.json();
+
+     console.log(data);
+
+      // console.log(data.alarmCount[0].count);
+  };
+  
+  useEffect(() => {
+    alarm();
+  }, []);
+  useEffect(() => {
+    let test = testData.reduce((accumulator, currentObject) => accumulator + currentObject['count'], 0);
+    setSum(test);
+  }, [testData]);
+  
+
+
+  let alarmcount=sum;
   logintest();
   return (
     // localStorage.getItem("isLoggedin") === "1" ? (
-    <Outlet />
+    <Outlet context={{alarmcount}}/>
   );
   // ) : (
   //   <Navigate to="/login" replace state={{ from: location }} />
