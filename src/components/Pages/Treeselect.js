fab23d41f22150a28bf8978363a6fa96002da0a3
DESKTOP-DFDSFOM\User <bjh2250@gmail.com>
2023-05-26 21:58:52 +0900

    전체검색 페이징 버튼 추가

--- src/components/Pages/Treeselect.js
+++ src/components/Pages/Treeselect.js
@@ -6,47 +6,65 @@ import { element } from "prop-types";
 
 const Treeselect = () => {
   const [trees, settrees] = useState([]);
+  const [comeTrees, setComeTrees] = useState([]);
   const [page, setPage] = useState(1);
-  const [URL, setURL] = useState("/forest");
-  async function fetchtrees() {
-    if (page !== 1) setURL(`/forest?page=${page}`);
-    else setURL("/forest");
+  const [disabled, setDisabled] = useState(false);
+
+  async function fetchtrees(URL) {
     const response = await fetch(URL);
     try {
       if (!response.ok) {
         throw new Error("Failed to fetch trees");
       }
       const data = await response.json();
+
       if (!data) {
         throw new Error("No Search Data");
       }
-      const mapping = data.data.map((element) => {
+      const mapping = await data.data.map((element) => {
         return {
           treeKey: element.treeKey,
           title: element.title,
+          dataRange: element.dataRange,
           description: element.description,
           tags: element.tags,
         };
       });
-      //settrees(filteredData)
-      //console.log(trees);
-      const comeTrees = trees;
-      comeTrees.push(...mapping);
-      settrees(comeTrees);
-      console.log(trees);
-      //console.log(mapping);
+
+      if (mapping[0]) {
+        setComeTrees(trees);
+        comeTrees.push(...mapping);
+        settrees(comeTrees);
+        console.log(trees);
+      } else {
+        console.log("예아");
+        setDisabled(true);
+      }
     } catch (e) {
       alert(e);
     }
   }
+  const nextLoad = () => {
+    const nextPage = page + 1;
+    setPage(nextPage);
+  };
 
   useEffect(() => {
-    setPage(1);
-    settrees([]);
-    fetchtrees();
-  }, []);
-  //console.log(trees);
-  if (trees !== null)
+    if (page === 1) {
+      setDisabled(false);
+    }
+    var URL = "/forest";
+    if (page !== 1) {
+      URL = `/forest?page=${page}`;
+    } else {
+      URL = "/forest";
+    }
+    console.log(URL);
+    fetchtrees(URL);
+  }, [page]);
+  if (page === 1 && disabled === true)
+    return <>아직 게시글이 없어요. 첫번째 게시글의 주인공이 되어보세요!</>;
+  else
     return (
       <>
         <div className="recbox">
@@ -54,14 +72,17 @@ const Treeselect = () => {
             <Rec2
               key={Math.random()}
               treeKey={post.treeKey}
+              dataRange={post.dataRange}
               title={post.title}
               description={post.description}
               tags={post.tags}
             ></Rec2>
           ))}
+          <button onClick={nextLoad} disabled={disabled}>
+            다음
+          </button>
         </div>
       </>
     );
-  else return <div>no data</div>;
 };
 export default Treeselect;
