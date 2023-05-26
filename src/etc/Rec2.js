2bc1b9491228f7c6aaf3fb1e81fcaca1b7ee4ab7
DESKTOP-DFDSFOM\User <bjh2250@gmail.com>
2023-05-26 18:23:30 +0900

    디자인 수정 필요

--- src/etc/Rec2.js
+++ src/etc/Rec2.js
@@ -2,7 +2,7 @@ import { useState, useEffect, Children } from "react";
 import { Link } from "react-router-dom";
 import tagData from "../dataJson/tagdata.json";
 import "./Rec2.css";
-const Rec2 = ({ treeKey, title, description, tags }) => {
+const Rec2 = ({ treeKey, title, description, tags, dataRange }) => {
   const [isSelected, setIsSelected] = useState(false);
   const tagMap = tagData.tags.map((e) => [e.value, e.text]);
   const tag = [];
@@ -10,11 +10,26 @@ const Rec2 = ({ treeKey, title, description, tags }) => {
     tags.forEach((v) => {
       tag.push("#" + tagMap[v - 1][1]);
     });
-
+  const nickName = dataRange.nickname;
+  const getStudentId = () => {
+    if (dataRange.hasOwnProperty("studentId")) {
+      return dataRange.studentId;
+    }
+    return "비공개";
+  };
+  const getDepartment = () => {
+    if (dataRange.hasOwnProperty("department")) {
+      return dataRange.department;
+    }
+    return "비공개";
+  };
   return (
     <div className={`rec2 ${isSelected ? "selected" : ""}`}>
       <Link to={`/tree/${treeKey}`} style={{ textDecoration: "none" }}>
-        <div>{title}</div>
+        <h3>{title}</h3>
+        <div>작성자:{nickName}</div>
+        <div>학번:{getStudentId()}</div>
+        <div>학과:{getDepartment()}</div>
         <div>{description}</div>
         <i>{tag} </i>
       </Link>
