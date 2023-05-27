b5afe8ac1e572338436e5331874ce1209815fe6b
Bae jung hyeon <bjh2250@gmail.com>
2023-05-28 02:07:41 +0900

    component / ui / loading 폴더 안에 로딩창 컴포넌트 작성 <Pulse isLoading={핸들함수}>로딩 메시지</Pulse> 로 작동 제어 가능

---
+++ src/components/ui/loading/Pulse.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { PulseLoader } from "react-spinners";
+import ReactModal from "react-modal";
+
+function Pulse(props) {
+  return (
+    <>
+      <ReactModal
+        isOpen={props.isLoading}
+        style={{
+          overlay: {
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(255, 255, 255, 0)",
+          },
+          content:{
+            outline:"none",
+            padding:0,
+            width:0,
+            height:0,
+            backgroundColor: "rgba(255, 255, 255, 0)"
+          }
+        }}
+        ariaHideApp={false}
+      >
+        <div
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          backdropFilter:"blur(100%)"
+        }}
+      >
+        <PulseLoader
+          color="#C63DEE"
+          height={15}
+          width={5}
+          radius={2}
+          margin={2}
+        />
+        <p style={{ marginTop: "40px", color: "rgb(1B1D1D)", fontWeight: "bold" }}>{props.children}</p>
+      </div>
+      </ReactModal>
+    </>
+  );
+}
+export default Pulse
